4209463d9b8c5089ef27b777cca265b06bd2bb82
Kennedy Kithome <kithomeken@gmail.com>
2024-04-17 17:33:58 +0300

    Common Routes Guard

---
+++ src/lib/guards/CommonRoutesGuard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppSelector } from "../../store/hooks";
+import { standardErrorRoutes } from "../../routes/errorRoutes";
+import { revokeAuthSession } from "../../store/auth/firebaseAuthActions";
+import Auth from "./Auth";
+
+const CommonRoutesGuard = () => {
+    const dispatch: any = useDispatch();
+    const auth0 = useAppSelector(state => state.auth0);
+    const sessionState = Auth.checkAuthentication(auth0);
+
+    if (!sessionState.authenticated) {
+        if (sessionState.status.resetSession) {
+            dispatch(revokeAuthSession());
+            return null;
+        } else {
+            return <Navigate to="/auth/sign-in" replace />;
+        }
+    } else {
+        if (sessionState.status.disabled) {
+            const suspendAccountRoute: any = (
+                standardErrorRoutes.find(
+                    (routeName) => routeName.name === 'SUSP_ACC')
+            )?.path
+
+            return <Navigate to={suspendAccountRoute} replace />;
+        }
+    }
+
+    return <Outlet />;
+};
+
+export default CommonRoutesGuard;
