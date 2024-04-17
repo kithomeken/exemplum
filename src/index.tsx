f9686388e301cd091ae50dc86a11021ab85654c3
Kennedy Kithome <kithomeken@gmail.com>
2024-04-17 11:55:18 +0300

    Redux State Injection

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { persistor, store } from './store/store';
+import * as serviceWorker from './serviceWorker';
+import { Loading } from './components/modules/Loading';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
     <React.StrictMode>
 
-        <App />
+        <Provider store={store}>
+
+            <PersistGate loading={<Loading />} persistor={persistor}>
+
+                <App />
+
+            </PersistGate>
+
+        </Provider>
 
     </React.StrictMode>,
     document.getElementById('root')
 );
 
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
