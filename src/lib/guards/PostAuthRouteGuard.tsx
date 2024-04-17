dbd2053350301968ffac457bbaeff30440bf6f01
Kennedy Kithome <kithomeken@gmail.com>
2024-04-17 17:34:11 +0300

    Post Authentication Routes Guard

---
+++ src/lib/guards/PostAuthRouteGuard.tsx
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import { useLocation, Navigate, Outlet } from "react-router";
+
+import Auth from "./Auth";
+import { useAppSelector } from "../../store/hooks";
+import { revokeAuthSession } from "../../store/auth/firebaseAuthActions";
+
+export default function PostAuthRouteGuard() {
+    const dispatch: any = useDispatch();
+    const location: any = useLocation();
+    const currentLocation = location.pathname;
+
+    const auth0 = useAppSelector(state => state.auth0);
+    const sessionState = Auth.checkAuthentication(auth0);
+
+    const state = { from: currentLocation };
+
+    if (auth0.sso) {
+        if (sessionState.identity) {
+            const locationState = location.state || {};
+            const redirectTo = locationState.from || "/home";
+            return <Navigate to={redirectTo} replace />;
+        }
+    } else {
+        if (sessionState.status.resetSession) {
+            dispatch(revokeAuthSession());
+            return;
+        } else {
+            return <Navigate to="/auth/sign-in" replace state={state} />;
+        }
+    }
+
+    return <Outlet />;
+}
