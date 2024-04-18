48760bec34d2179eaacc10e0749c8ec429f18d7d
Kennedy Kithome <kithomeken@gmail.com>
2024-04-18 16:30:48 +0300

    Post Auth Routes Guard Update

--- src/lib/guards/PostAuthRouteGuard.tsx
+++ src/lib/guards/PostAuthRouteGuard.tsx
@@ -8,17 +8,18 @@ import { revokeAuthSession } from "../../store/auth/firebaseAuthActions";
 export default function PostAuthRouteGuard() {
     const dispatch: any = useDispatch();
     const location: any = useLocation();
+
     const currentLocation = location.pathname;
+    const state = { from: currentLocation };
 
     const auth0 = useAppSelector(state => state.auth0);
-    const sessionState = Auth.checkAuthentication(auth0);
-
-    const state = { from: currentLocation };
+    const sessionState = Auth.checkAuthentication(auth0);    
 
     if (auth0.sso) {
         if (sessionState.identity) {
             const locationState = location.state || {};
             const redirectTo = locationState.from || "/home";
+            
             return <Navigate to={redirectTo} replace />;
         }
     } else {
