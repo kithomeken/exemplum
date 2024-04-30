26d123cc05aee1e619b5defe5751b776c4c46ced
Kennedy Kithome <kithomeken@gmail.com>
2024-04-30 15:17:48 +0300

    Settings Route Guard Update

--- src/lib/guards/SettingsRoutesGuard.tsx
+++ src/lib/guards/SettingsRoutesGuard.tsx
@@ -46,7 +46,7 @@ export default function SettingsRoutesGuard() {
                 standardErrorRoutes.find(
                     (routeName) => routeName.name === 'IDENTITY_VERF_')
             )?.path
-            
+
             return <Navigate to={identityVerificationRoute} replace />;
         }
     }
@@ -69,7 +69,7 @@ export default function SettingsRoutesGuard() {
         return null;
     };
 
-    const Ps0tMenuLink = (menu, sT0) => {
+    const Ps0tMenuLink = (menu: any, sT0: any) => {
         const Ps0t = routePs0t(location.pathname);
         return (
             <Link
@@ -91,30 +91,33 @@ export default function SettingsRoutesGuard() {
     return (
         <div>
             <div className="flex h-screen">
+
                 <Header />
+
                 <div className="flex flex-col w-full h-screen">
                     <div className="w-full overflow-y-auto" style={CONFIG_MARGIN_TOP}>
                         <div className="kiOAkj">
-                            <div className="w-full bg-gradient-to-r from-amber-100 to-emerald-100 h-16">
-                                <div className="kiOAkj sttng_strp h-16 px-12"></div>
-                            </div>
                             <div className="flex flex-col w-full md:flex-row overflow-hidden">
                                 <div className="flex-shrink-0 w-full md:w-56">
                                     <div className="p-4">
-                                        <h1 className="text-xl text-amber-600 font-medium tracking-wider">
+                                        <h1 className="text-xl text-stone-600 font-medium tracking-wider">
                                             Settings
                                         </h1>
                                     </div>
+
                                     <nav className="flex flex-col px-3">
                                         {Ps0tMenuLink("Account Profile", "CNF_ACC_ID_")}
                                         {Ps0tMenuLink("Change Email", "CNF_EMAIL_CHNG_")}
                                         {Ps0tMenuLink("Entity", "CNF_ENTITY_")}
                                     </nav>
                                 </div>
-                                <div className="flex-1 overflow-hidden py-3 h-screen -mt-32 pt-32">
+
+                                <div className="flex-1 overflow-hidden py-3 h-screen pt-20 -mt-16">
                                     <main className="flex-1 h-full px-4 overflow-y-auto border-gray-200 md:border-l">
-                                        <div className="container h-full mx-auto border-t md:border-t-0 pb-16">
+                                        <div className="container h-full mx-auto border-t md:border-t-0 pb-16 px-2 py-2">
+
                                             <Outlet />
+
                                         </div>
                                     </main>
                                 </div>
