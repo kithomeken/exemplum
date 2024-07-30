136e9ebbecf1481d0bc32430c21682b23e18f5f5
Kennedy Kithome <kithomeken@gmail.com>
2024-07-30 18:44:10 +0300

    Password Policy Amendment

--- src/views/auth/PasswordPolicy.tsx
+++ src/views/auth/PasswordPolicy.tsx
@@ -15,10 +15,10 @@ export const PasswordPolicy: FC<Basic_Modal_Props> = ({ show, showOrHide }) => {
                 details={
                     <>
                         <p className="text-sm text-gray-700 mb-6">
-                            For security purposes, your password must meet the following criteria:
+                            To ensure security of your account and unauthorized access, your password must meet the following criteria:
                         </p>
 
-                        <ul className="list-disc ml-6 mb-6">
+                        <ul className="list-disc ml-6 mb-6 text-sm gap-y-1 flex flex-col">
                             <li>At least 8 characters long</li>
                             <li>Contains at least one uppercase letter (A-Z)</li>
                             <li>Contains at least one lowercase letter (a-z)</li>
@@ -28,11 +28,6 @@ export const PasswordPolicy: FC<Basic_Modal_Props> = ({ show, showOrHide }) => {
                                 !@#$%^&*()-_=+[]{ }|;:'",.&lt;?/~)
                             </li>
                         </ul>
-
-                        <p className="text-sm text-gray-700">
-                            Please ensure your password meets these requirements to enhance the
-                            security of your account. Thank you!
-                        </p>
                     </>
                 }
             />
