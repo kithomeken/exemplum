e5ce8efcf50f485a7ede6ed44abdfa90a33fbd94
Kennedy Kithome <kithomeken@gmail.com>
2024-08-06 17:41:54 +0300

    Entity Properties Update

--- src/views/settings/EntityMembers.tsx
+++ src/views/settings/EntityMembers.tsx
@@ -3,6 +3,7 @@ import { toast } from "react-toastify"
 
 import { ERR_404 } from "../errors/ERR_404"
 import { ERR_500 } from "../errors/ERR_500"
+import { useAppSelector } from "../../store/hooks"
 import { ACCOUNT, AUTH } from "../../api/API_Registry"
 import HttpServices from "../../services/HttpServices"
 import { Loading } from "../../components/modules/Loading"
@@ -24,6 +25,8 @@ export const EntityMembers = () => {
         fetchEntityMembers()
     }, [])
 
+    const auth0: any = useAppSelector(state => state.auth0)
+
     const fetchEntityMembers = async () => {
         let { httpStatus } = state
         let { status } = state
@@ -36,6 +39,14 @@ export const EntityMembers = () => {
             if (response.data.success) {
                 status = 'fulfilled'
                 data = response.data.payload
+
+                const sortedUsers = response.data.payload.onB1.sort((a: any, b: any) => {
+                    if (a.uid === auth0.identity.uid) return -1;
+                    if (b.uid === auth0.identity.uid) return 1;
+                    return 0;
+                });
+
+                data.onB1 = sortedUsers
             } else {
                 status = 'rejected'
             }
@@ -66,8 +77,8 @@ export const EntityMembers = () => {
     }
 
     const resendEmailInvitation = async () => {
-        let {posting} = state
-        let {invitation} = state
+        let { posting } = state
+        let { invitation } = state
 
         try {
             let formData = new FormData()
@@ -135,67 +146,58 @@ export const EntityMembers = () => {
                 ) : state.status === 'fulfilled' ? (
                     <>
                         <div className="flex-none w-full py-4">
-
-
                             <ul role="list" className="divide-y divide-stone-100">
                                 {
                                     state.data.onB1.map((person: any) => (
-                                        <li key={person.acid} className="flex justify-between gap-x-6 py-2">
-                                            <div className="flex min-w-0 gap-x-4 items-center align-middle">
-                                                {
-                                                    person.photo_url ? (
-                                                        <>
-                                                            <img className="h-10 w-10 flex-none rounded-full bg-stone-50" src={person.photo_url} alt="" />
-                                                        </>
-                                                    ) : (
-                                                        <>
-                                                            {
-                                                                person.display_name.length > 1 ? (
-                                                                    <div className={`w-10 h-10 flex items-center justify-center rounded-full ${getColorForLetter(person.display_name.charAt(0))}`}>
-                                                                        <span className="text-white text-lg">
-                                                                            {person.display_name.charAt(0)}
-                                                                        </span>
-                                                                    </div>
-                                                                ) : (
-                                                                    <div className={`w-10 h-10 flex items-center justify-center rounded-full ${getColorForLetter('X')}`}>
-                                                                        <span className="text-white text-lg">
-                                                                            X
-                                                                        </span>
-                                                                    </div>
-                                                                )
-                                                            }
-                                                        </>
-                                                    )
-                                                }
+                                        <>
+                                            <li key={person.acid} className="flex justify-between gap-x-6 py-2">
+                                                <div className="flex min-w-0 gap-x-4 items-center align-middle">
+                                                    {
+                                                        person.photo_url ? (
+                                                            <>
+                                                                <img className="h-10 w-10 flex-none rounded-full bg-stone-50" src={person.photo_url} alt="" />
+                                                            </>
+                                                        ) : (
+                                                            <>
+                                                                {
+                                                                    person.display_name.length > 1 ? (
+                                                                        <div className={`w-10 h-10 flex items-center justify-center rounded-full ${getColorForLetter(person.display_name.charAt(0))}`}>
+                                                                            <span className="text-white text-lg">
+                                                                                {person.display_name.charAt(0)}
+                                                                            </span>
+                                                                        </div>
+                                                                    ) : (
+                                                                        <div className={`w-10 h-10 flex items-center justify-center rounded-full ${getColorForLetter('X')}`}>
+                                                                            <span className="text-white text-lg">
+                                                                                X
+                                                                            </span>
+                                                                        </div>
+                                                                    )
+                                                                }
+                                                            </>
+                                                        )
+                                                    }
 
-                                                <div className="min-w-0 flex-auto">
-                                                    <p className="leading-6 text-stone-900">{person.display_name}</p>
-                                                    <p className="mt-1 truncate text-xs leading-5 text-stone-500">{person.email}</p>
+                                                    <div className="min-w-0 flex-auto">
+                                                        <p className="leading-6 text-stone-900">{person.display_name}</p>
+                                                        <p className="mt-1 truncate text-xs leading-5 text-stone-500">{person.email}</p>
+                                                    </div>
                                                 </div>
-                                            </div>
 
-                                            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-                                                <p className="text-sm leading-6 text-stone-900">{person.name}</p>
-
-                                                {
-                                                    person.status === 'Y' ? (
-                                                        <div className="mt-1 flex items-center gap-x-1.5">
-                                                            <div className="flex-none rounded-full bg-emerald-500/20 p-1 animate-pulse">
-                                                                <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                                                <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+                                                    {
+                                                        person.status !== 'Y' ? (
+                                                            <div className="mt-1 flex items-center gap-x-1.5">
+                                                                <div className="flex-none rounded-full bg-red-500/20 p-1">
+                                                                    <div className="h-1.5 w-1.5 rounded-full bg-red-500" />
+                                                                </div>
+                                                                <p className="text-xs leading-5 text-red-500">Disabled</p>
                                                             </div>
-                                                            <p className="text-xs leading-5 text-emerald-500">Active</p>
-                                                        </div>
-                                                    ) : (
-                                                        <div className="mt-1 flex items-center gap-x-1.5">
-                                                            <div className="flex-none rounded-full bg-red-500/20 p-1">
-                                                                <div className="h-1.5 w-1.5 rounded-full bg-red-500" />
-                                                            </div>
-                                                            <p className="text-xs leading-5 text-red-500">Disabled</p>
-                                                        </div>
-                                                    )
-                                                }
-                                            </div>
-                                        </li>
+                                                        ) : null
+                                                    }
+                                                </div>
+                                            </li>
+                                        </>
                                     ))
                                 }
                             </ul>
