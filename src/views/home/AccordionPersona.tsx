094641b9e27304fdc61e7e4a878e0eda108a0cb5
Kennedy Kithome <kithomeken@gmail.com>
2024-09-06 13:06:02 +0300

    Padding Change

--- src/views/home/AccordionPersona.tsx
+++ src/views/home/AccordionPersona.tsx
@@ -28,7 +28,7 @@ export const AccordionPersona: FC<props> = ({ activeElement, persona, attachment
     return (
         <React.Fragment>
             <TECollapse show={activeElement === "persona"} className="!mt-0 !rounded-b-none !shadow-none">
-                <div className="md:px-5 py-4">
+                <div className="md:px-5 pb-4">
                     <div className="flex flex-col">
                         <div className="px-4 sm:px-0">
                             <p className="mt-1 max-w-2xl text-sm leading-6 text-stone-500">
@@ -77,7 +77,7 @@ export const AccordionPersona: FC<props> = ({ activeElement, persona, attachment
                                             <li className="flex md:flex-row flex-col items-center md:justify-between py-4 pl-4 pr-5 text-sm leading-6">
                                                 <div className="flex md:w-0 w-full flex-1 md:items-center">
                                                     <svg className="h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                                        <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
+                                                        <path fillRule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clipRule="evenodd" />
                                                     </svg>
 
                                                     <div className="ml-4 flex min-w-0 flex-1 gap-2">
