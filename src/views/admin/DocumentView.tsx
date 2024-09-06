a41885051edb5320d61884f6ad202d4eeaba3ef5
Kennedy Kithome <kithomeken@gmail.com>
2024-09-06 15:44:54 +0300

    Document Viewer Design Refactor

--- src/views/admin/DocumentView.tsx
+++ src/views/admin/DocumentView.tsx
@@ -43,14 +43,16 @@ export const DocumentView: FC<Basic_Modal_Props> = ({ show, showOrHide, uuid }) 
                             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         >
                             <div className={`inline-block align-bottom bg-white sm:max-w-${size} rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full`}>
-                                <div className="bg-white pt-5 pb-4 sm:py-6 sm:pb-4">
-                                    <div className="w-full">
-                                        <div className="sm:col-span-8 lg:col-span-7 mb-3 px-4 sm:px-6">
-                                            <h2 className="text-xl text-orange-600">
-                                                Document View
-                                            </h2>
+                                {
+                                    isLoading && (
+                                        <div className="pt-2 px-6">
+                                            <Loading />
                                         </div>
+                                    )
+                                }
 
+                                <div className="swqRE" style={{ display: isLoading ? 'none' : 'block', width: '100%', height: 'auto' }}>
+                                    <div className="w-full">
                                         <div className="w-full max-h-96 overflow-y-auto px-0 sm:px-0">
                                             <div style={{ position: 'relative', width: '100%', height: '100%' }}>
                                                 {
@@ -63,21 +65,21 @@ export const DocumentView: FC<Basic_Modal_Props> = ({ show, showOrHide, uuid }) 
                                                     )
                                                 }
 
-                                                <img className="rounded-md max-w-full h-56" src={`${FQDN}/files/documents/${uuid}`} alt={uuid}
+                                                <img className="rounded-md max-w-full h-56 block" src={`${FQDN}/files/documents/${uuid}`} alt={uuid}
                                                     onLoad={handleImageLoaded}
-                                                    style={{ display: isLoading ? 'none' : 'block', width: '100%', height: 'auto' }}
+                                                    style={{ width: '100%', height: 'auto' }}
                                                 />
                                             </div>
                                         </div>
                                     </div>
-                                </div>
 
-                                <div className="bg-slate-100 px-4 py-4 sm:px-6 sm:flex sm:flex-row-reverse">
-                                    <div className="w-full space-y-px">
-                                        <div className="flex flex-row-reverse items-center align-middle">
-                                            <button type="button" className="w-24 justify-center disabled:cursor-not-allowed text-sm rounded-md border border-transparent shadow-sm px-3 py-1-5 bg-orange-600 text-white disabled:bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:ring-orange-500" onClick={showOrHide}>
-                                                Close
-                                            </button>
+                                    <div className="bg-slate-100 px-4 py-4 sm:px-6 sm:flex sm:flex-row-reverse">
+                                        <div className="w-full space-y-px">
+                                            <div className="flex flex-row-reverse items-center align-middle">
+                                                <button type="button" className="w-24 justify-center disabled:cursor-not-allowed text-sm rounded-md border border-transparent shadow-sm px-3 py-1-5 bg-orange-600 text-white disabled:bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-0 focus:ring-offset-2 focus:ring-orange-500" onClick={showOrHide}>
+                                                    Close
+                                                </button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
