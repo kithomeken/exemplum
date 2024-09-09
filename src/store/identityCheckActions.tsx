e4742f5fe019848d345e568b797f8e3c136fb192
Kennedy Kithome <kithomeken@gmail.com>
2024-09-09 18:07:17 +0300

    Artist Entity Details Modification

--- src/store/identityCheckActions.tsx
+++ src/store/identityCheckActions.tsx
@@ -385,6 +385,61 @@ export function artistEntityCreation(propsIn: IdentityProps) {
     }
 }
 
+export function artistEntityModification(propsIn: IdentityProps) {
+    return async (dispatch: (arg0: { type: string; response: any }) => void) => {
+        const IdentityProps = { ...propsIn }
+
+        dispatch({
+            type: IDENTITY_.PROCESSING,
+            response: {
+                redirect: false,
+            },
+        });
+
+        try {
+            let formData = new FormData()
+            const dataDump = IdentityProps.dataDump
+            const entityHash = StorageServices.getLocalStorage(STORAGE_KEYS.ENTITY_HASH)
+
+            formData.append('bio', dataDump.bio)
+            formData.append('type', dataDump.type)
+            formData.append('entity', dataDump.entity)
+            formData.append('artist', dataDump.artist)
+
+            if (entityHash !== null && entityHash !== undefined) {
+                formData.append('hash', entityHash);
+            }
+
+            const identityResponse: any = await HttpServices.httpPut(AUTH.ID_META_03, formData)
+
+            if (identityResponse.data.success) {
+                // Save the entity type to local storage
+                StorageServices.setLocalStorage(STORAGE_KEYS.ENTITY_TYPE, dataDump.specificObject)
+
+                dispatch({
+                    type: AUTH_.ID_META_03,
+                    response: identityResponse.data.payload,
+                });
+
+                dispatch({
+                    type: IDENTITY_.PRc0_OVRD_END,
+                    response: 'PRc0',
+                });
+            } else {
+                dispatch({
+                    type: IDENTITY_.PRc0_EXCEPTION,
+                    response: identityResponse.data,
+                });
+            }
+        } catch (error) {
+            dispatch({
+                type: IDENTITY_.PRc0_EXCEPTION,
+                response: error,
+            });
+        }
+    }
+}
+
 export function resetIdentity() {
     return (dispatch: (arg0: { type: string; response: any }) => void) => {
         dispatch({
