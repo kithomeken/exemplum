1f8ff1fb5b0e1d7ab6d7906e402a321f8425b130
Kennedy Kithome <kithomeken@gmail.com>
2024-10-23 08:19:58 +0300

    New Header Menus

--- src/views/ingress/LandingHeader.tsx
+++ src/views/ingress/LandingHeader.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import React, { useEffect, useState } from "react"
 
-import { APPLICATION } from "../../global/ConstantsRegistry"
+import colorfulLogo from "../../assets/images/1akbR3BuvCSqw5uGy.svg"
 
 export const LandingHeader = () => {
     const [scrolled, setScrolled] = useState(false);
@@ -28,8 +28,14 @@ export const LandingHeader = () => {
             <nav className={`fixed z-10 top-0 left-0 w-full transition duration-300 ${scrolled ? 'backdrop-blur-md bg-white/70' : 'bg-transparent'}`} >
                 <div className="w-full mx-auto md:py-2 py-1.5 container md:max-w-4xl lg:max-w-4xl xl:max-w-5xl">
                     <div className="max-w-full mx-auto md:px-4 px-2">
-                        <div className="relative flex items-center justify-between h-16">
-                            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                        <div className="relative flex items-center justify-between md:h-24 h-16">
+                            <div className="absolute inset-y-0 left-0 flex w-full items-center sm:hidden">
+                                <div className="flex-grow">
+                                    <Link to="/" className="mb-0 font-medium px-0 rounded-md" aria-current="page">
+                                        <img className="h-12" src={colorfulLogo} loading="lazy" alt="colorful_logo" />
+                                    </Link>
+                                </div>
+
                                 <button
                                     type="button"
                                     onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -77,20 +83,36 @@ export const LandingHeader = () => {
                             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="hidden sm:block">
                                     <div className="flex space-x-4">
-                                        <Link to="/" className="text-orange-500 mb-0 font-medium px-0 py-2 rounded-md" aria-current="page">
-                                            <span className="text-3xl">{APPLICATION.NAME}</span>
+                                        <Link to="/" className="mb-0 font-medium px-0 py-2 rounded-md" aria-current="page">
+                                            <img className="h-24  py-3 mx-auto" src={colorfulLogo} loading="lazy" alt="colorful_logo" />
                                         </Link>
                                     </div>
                                 </div>
                             </div>
 
                             <div className="hidden sm:flex absolute inset-y-0 right-0 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 gap-x-6">
-                                <Link to="/auth/sign-in" className="text-orange-600 hover:text-orange-700">
-                                    Sign In
+                                <Link to="#" className="text-gray-600 hover:text-gray-700">
+                                    How it Works
                                 </Link>
 
-                                <Link to="/auth/sign-up" className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600">
-                                    Create an Account
+                                <Link to="#" className="text-gray-600 hover:text-gray-700">
+                                    FAQs
+                                </Link>
+
+                                <Link to="#" className="text-gray-600 hover:text-gray-700">
+                                    About Us
+                                </Link>
+
+                                <Link to="#" className="text-gray-600 hover:text-gray-700">
+                                    Contact Us
+                                </Link>
+
+                                <Link to="#" className="text-gray-600 hover:text-gray-700 flex gap-x-3 items-center align-middle">
+                                    <span className="fa-regular fa-search"></span> Search
+                                </Link>
+
+                                <Link to="/auth/sign-in" className="text-orange-600 hover:text-orange-700">
+                                    Sign In
                                 </Link>
                             </div>
                         </div>
@@ -99,15 +121,33 @@ export const LandingHeader = () => {
 
                 {
                     isMenuOpen && (
-                        <div className="sm:hidden absolute top-16 left-0 w-full bg-white shadow-lg px-3 pb-2">
-                            <div className="flex flex-col items-start space-y-4 py-4 px-6">
-                                <Link to="/auth/sign-in" className="text-gray-600 hover:text-gray-700 hover:underline focus:underline" onClick={() => setIsMenuOpen(false)}>
-                                    Sign In
+                        <div className="sm:hidden absolute top-[76px] left-0 w-full bg-white shadow-lg px-3 pb-2">
+                            <div className="flex flex-col items-start space-y-4 py-4 px-6 w-full">
+                                <Link to="#" className="text-gray-600 hover:text-gray-700 hover:underline focus:underline" onClick={() => setIsMenuOpen(false)}>
+                                    How it Works
                                 </Link>
 
-                                <Link to="/auth/sign-up" className="text-orange-600 hover:text-gray-700 hover:underline focus:underline" onClick={() => setIsMenuOpen(false)}>
-                                    Create an Account
+                                <Link to="#" className="text-gray-600 hover:text-gray-700 hover:underline focus:underline" onClick={() => setIsMenuOpen(false)}>
+                                    FAQs
                                 </Link>
+
+                                <Link to="#" className="text-gray-600 hover:text-gray-700 hover:underline focus:underline" onClick={() => setIsMenuOpen(false)}>
+                                    About Us
+                                </Link>
+
+                                <Link to="#" className="text-gray-600 hover:text-gray-700 hover:underline focus:underline" onClick={() => setIsMenuOpen(false)}>
+                                    Contact Us
+                                </Link>
+
+                                <Link to="#" className="text-gray-600 hover:text-gray-700 hover:underline focus:underline flex gap-x-3 items-center align-middle" onClick={() => setIsMenuOpen(false)}>
+                                    <span className="fa-regular fa-search"></span> Search
+                                </Link>
+
+                                <div className="border-t pt-3 w-full">
+                                    <Link to="/auth/sign-in" className="text-orange-600 hover:text-gray-700 hover:underline focus:underline" onClick={() => setIsMenuOpen(false)}>
+                                        Sign In
+                                    </Link>
+                                </div>
                             </div>
                         </div>
                     )
