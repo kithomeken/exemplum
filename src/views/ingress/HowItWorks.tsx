e2c5d1e7f1d523e27f1c79de446045d9512a9142
Kennedy Kithome <kithomeken@gmail.com>
2024-10-23 15:09:34 +0300

    Favicon.co

--- src/views/ingress/HowItWorks.tsx
+++ src/views/ingress/HowItWorks.tsx
@@ -25,7 +25,7 @@ export const HowItWorks = () => {
                                 <div>
                                     <div className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-orange-700"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                                         className="h-6 w-6 text-orange-700">
                                         <path d="M12 5l0 14"></path>
                                         <path d="M18 13l-6 6"></path>
@@ -50,7 +50,7 @@ export const HowItWorks = () => {
                                 <div>
                                     <div className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-orange-700"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                                         className="h-6 w-6 text-orange-700">
                                         <path d="M12 5l0 14"></path>
                                         <path d="M18 13l-6 6"></path>
@@ -75,7 +75,7 @@ export const HowItWorks = () => {
                                 <div>
                                     <div className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-orange-700"><svg
                                         xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                                         className="h-6 w-6 text-orange-700">
                                         <path d="M12 5l0 14"></path>
                                         <path d="M18 13l-6 6"></path>
@@ -100,7 +100,7 @@ export const HowItWorks = () => {
                                     <div
                                         className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-orange-700 bg-orange-700">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                                             className="h-6 w-6 text-white">
                                             <path d="M5 12l5 5l10 -10"></path>
                                         </svg>
